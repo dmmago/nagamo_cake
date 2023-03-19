4edbf3bd7799f2175c2e877ca625a67890b9ba0c
EC2 Default User <ec2-user@ip-172-31-40-117.ap-northeast-1.compute.internal>
2023-03-19 15:09:04 +0000

    adminitem

--- config/routes.rb
+++ config/routes.rb
@@ -2,22 +2,22 @@ Rails.application.routes.draw do
  devise_for :admin,skip: [:registrations, :passwords], controllers: {
   sessions: "admin/sessions"
 }
-  
+
   devise_for :customers,skip: [:passwords], controllers: {
   registrations: "public/registrations",
   sessions: 'public/sessions'
  }
   namespace :admin do
     root to: 'homes#top'
-    
+
     resources :customers, only: [:index, :show, :edit, :update]
     resources :genres, only: [:index, :create, :edit, :update]
     resources :items, only: [:new, :index, :create, :show, :edit, :update]
     resources :orders, only: [:update, :show]
     resources :order_details, only: [:update]
   end
-   
-  
+
+
    scope module: :public do
     get "/" => "homes#top"
     get '/about' => 'homes#about', as: 'about'
@@ -31,10 +31,10 @@ Rails.application.routes.draw do
     get 'orders/comfirm'#post
     get 'orders/complete'
     resources :addresses, only: [:index, :edit, :create, :update, :destroy]
-    
+
   end
-  
- 
+
+
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
  end
