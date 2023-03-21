9c47c655205d19761467638c6d5d2a171cd2d303
EC2 Default User <ec2-user@ip-172-31-40-117.ap-northeast-1.compute.internal>
2023-03-21 14:53:41 +0000

    cart_item

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -1,4 +1,15 @@
 class Public::CartItemsController < ApplicationController
   def index
+    @cart_items = CartItem.all
+  end
+
+  def create
+    @cart_item = CartItem.new(cart_item_params)
+    @cart_item.save
+  end
+
+ private
+  def cart_item_params
+      params.require(:cart_item).permit(:item_id, :amount)
   end
 end
