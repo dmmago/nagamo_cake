78ed8831c891f60aafbadb81b94177a07ca3bd39
EC2 Default User <ec2-user@ip-172-31-40-117.ap-northeast-1.compute.internal>
2023-03-22 10:33:44 +0000

    my_page

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -4,8 +4,10 @@ class Public::CartItemsController < ApplicationController
   end
 
   def create
-    @cart_item = CartItem.new(cart_item_params)
-    @cart_item.save
+    cart_item = CartItem.new(cart_item_params)
+    cart_item.customer_id = current_customer.id
+    cart_item.save
+    redirect_to cart_items_path
   end
 
  private
