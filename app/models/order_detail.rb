09f5ce26b116bf2dd5423cd40ce04880c90f0cd3
EC2 Default User <ec2-user@ip-172-31-40-117.ap-northeast-1.compute.internal>
2023-04-10 17:19:08 +0000

    bootstrap

--- app/models/order_detail.rb
+++ app/models/order_detail.rb
@@ -1,4 +1,9 @@
 class OrderDetail < ApplicationRecord
   belongs_to :item
   belongs_to :order
+  enum making_status: { cannot_be_manufactured: 0, waiting_for_production: 1, production: 2, completion_of_production: 3 } 
+def order_subtotal
+    item.with_tax_price * amount
+end
+
 end
